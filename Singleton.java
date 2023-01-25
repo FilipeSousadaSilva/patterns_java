5bddfa3d85bc426109b9a19a7be905f3cdf261cf
Filipe Sousa <filipess.2011@gmail.com>
2023-01-25 20:07:14 -0300

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,22 @@
+public class Singleton {
+    private static Singleton instance;
+    private Singleton() {}
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+public class SingletonExample {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        if (singleton1 == singleton2) {
+            System.out.println("Two variables contain the same instance.");
+        } else {
+            System.out.println("Two variables contain different instances.");
+        }
+    }
+}
