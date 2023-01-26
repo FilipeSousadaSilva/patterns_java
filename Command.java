9041999731b54b01906c2f40f0c3f8a8aeb62482
Filipe Sousa <filipess.2011@gmail.com>
2023-01-25 21:06:39 -0300

    Create Command.java

---
+++ Command.java
@@ -0,0 +1,65 @@
+interface Command {
+    void execute();
+}
+
+class Light {
+    public void turnOn() {
+        System.out.println("The light is on");
+    }
+
+    public void turnOff() {
+        System.out.println("The light is off");
+    }
+}
+
+class TurnOnLightCommand implements Command {
+    private Light light;
+
+    public TurnOnLightCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+class TurnOffLightCommand implements Command {
+    private Light light;
+
+    public TurnOffLightCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+public class CommandPattern {
+    public static void main(String[] args) {
+        Light light = new Light();
+        Command turnOnCommand = new TurnOnLightCommand(light);
+        Command turnOffCommand = new TurnOffLightCommand(light);
+
+        RemoteControl remoteControl = new RemoteControl();
+
+        remoteControl.setCommand(turnOnCommand);
+        remoteControl.pressButton();
+
+        remoteControl.setCommand(turnOffCommand);
+        remoteControl.pressButton();
+    }
+}
